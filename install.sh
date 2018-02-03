e0f5633c439b040d3bddf20dce7377aa626cf4e8
bzsklb <bzsklb@users.noreply.github.com>
2018-02-04 07:17:37 +0800

    Update install.sh
    autodetection of architecture, autodetection of Linux distributive, remove arm, versions update, other minor updates.

--- install.sh
+++ install.sh
@@ -1,89 +1,115 @@
 #!/bin/bash
 
-latest="v4.22-9634-beta"
-lateststable="v4.20-9608-rtm-2016.04.17"
-#Release Date: 2015-10-19
-initfile="vpnserver2"
+beta="v4.24-9652-beta-2017.12.21"
+lateststable="v4.25-9656-rtm-2018.01.15"
+#Release Date: 2018.01.15
+service="vpnserver.service"
 
 echo "--------------------------------------------------------------------"
 echo "SoftEther VPN Server Install script"
-echo "By AhmadShamli"
+echo "By AhmadShamli and bzsklb. Special for https://www.twitch.tv/knjazevdesu"
+echo "http://github.com/bzsklb"
 echo "http://github.com/AhmadShamli"
 echo "http://AhmadShamli.com"
 echo "credit: DigitalOcean and StackOverflow"
 echo "https://www.digitalocean.com/community/tutorials/how-to-setup-a-multi-protocol-vpn-server-using-softether"
+echo "https://linuxconfig.org/setting-up-softether-vpn-server-on-ubuntu-16-04-xenial-xerus-linux"
 echo "--------------------------------------------------------------------"
 echo "--------------------------------------------------------------------"
+
+if [ "$(id -u)" != "0" ]; then
+    echo "Please run as root/sudo."
+    exit 1
+fi
+
 echo
-echo "Select Architecture"
+echo "Architecture: "
+
+# Get the machine Architecture
+Architecture=$(uname -m)
+case "$Architecture" in
+    x86)    Architecture="x86"                  ;;
+    arm?????)   Architecture="arm"                 ;;
+    i?86)   Architecture="x86"                  ;;
+    amd64)  Architecture="x86_64"                    ;;
+    x86_64) Architecture="x86_64"                   ;;
+	mipsel)		Architecture="mipsel"					;;
+* ) #echo    "Your Architecture '$Architecture' -> ITS NOT SUPPORTED."   ;;
+esac
+
 echo
-echo " 1. Arm EABI (32bit)"
-echo " 2. Intel x86 (32bit)"
-echo " 3. Intel x64/AMD64 (64bit)"
-echo
-echo "Please choose architecture: "
-read tmp
+echo "Operating System Architecture : $Architecture"
 echo
 
-if test "$tmp" = "3"
+if test "$Architecture" = "x86_64"
 then
 	arch="64bit_-_Intel_x64_or_AMD64"
 	arch2="x64-64bit"
-	echo "Selected : 1 " $arch
-elif test "$tmp" = "2"
+	echo "Selected : " $arch
+elif test "$Architecture" = "x86"
 then
 	arch="32bit_-_Intel_x86"
 	arch2="x86-32bit"
-	echo "Selected : 2 " $arch
-elif test "$tmp" = "1"
+	echo "Selected : " $arch
+elif test "$Architecture" = "arm"
 then
-	arch="32bit_-_ARM_EABI"
-	arch2="arm_eabi-32bit"
-	echo "Selected : 3 " $arch
-else #default if non selected
-	arch="32bit_-_Intel_x86"
-	arch2="x86-32bit"
-	echo "Selected : 2 " $arch
+	echo "Selected : " $arch
+	echo    "Your Architecture '$Architecture' -> NOT SUPPORTED BY THIS SCRIPT."
+	exit 1
+elif test "$Architecture" = "mipsel"
+then
+	echo "Selected : " $arch
+	echo    "Your Architecture '$Architecture' -> NOT SUPPORTED BY THIS SCRIPT."
+	exit 1
+else
+	echo    "Your Architecture '$Architecture' -> NOT SUPPORTED BY THIS SCRIPT."
+	exit 1
 fi
 
 echo "--------------------------------------------------------------------"
 echo
 echo "Select OS"
 echo
-echo " 1. Debian/Ubuntu"
-echo " 2. CentOS/Fedora"
 echo
-echo "Please choose OS: "
-read tmp
 echo
-
-if test "$tmp" = "2"
-then
-	os="cent"
-	echo "Selected : 2 CentOS/Fedora"
+if VERB="$( command -v apt-get )" 2> /dev/null; then
+   echo "OS: Debian-based"
+   os="deb"
+elif VERB="$( command -v yum )" 2> /dev/null; then
+   echo "OS: Modern Red Hat-based"
+   os="cent"
+elif VERB="$( command -v pacman )" 2> /dev/null; then
+	echo "OS: Archlinux-based "
+	os="pacman"
+	echo "Sorry. Archlinux is currently not supported by this script. "
+   	exit 1
 else
-	os="deb"
-	echo "Selected : 1 Debian/Ubuntu"
+   echo "Sorry. Your linux distro is not supported." >&2
+   exit 1
 fi
-
+echo
 echo "--------------------------------------------------------------------"
 echo
 echo "Select build"
 echo
-echo " 1. latest(might include beta/rc)"
-echo " 2. latest stable"
+echo " 1. latest stable"
+echo " 2. latest(might include beta/rc)"
 echo
 echo "Please choose build: "
 read tmp
 echo
 
-if test "$tmp" = "2"
+if test "$tmp" = "1"
 then
 	version="$lateststable"
-	echo "Latest stable selected: 2 "$lateststable
+	echo "Latest stable selected: 1 "$lateststable
+elif test "$tmp" = "2"
+then
+	version="$beta"
+	echo "Latest build(stable/beta) selected: 1 "$beta
 else
-	version="$latest"
-	echo "Latest build(stable/beta) selected: 1 "$latest
+	version="$lateststable"
+	echo "Default: stable "$lateststable
 fi
 
 file="softether-vpnserver-"$version"-linux-"$arch2".tar.gz"
@@ -91,11 +117,11 @@ link="http://www.softether-download.com/files/softether/"$version"-tree/Linux/So
 
 if [ ! -s "$file" ]||[ ! -r "$file" ];then
 	#remove and redownload empty or unreadable file
-	rm -f "$link"
+	rm -f "$file"
 	wget "$link"
 elif [ ! -f "file" ];then
 	#download if not exist
-	wget "$file"
+	wget "$link"
 fi
 
 if [ -f "$file" ];then
@@ -110,13 +136,21 @@ else
 	break
 fi
 
-if [ "$os" -eq "cent" ];then
+if [ "$os" = "cent" ];then
 	yum upgrade
-	yum groupinstall "Development Tools" gcc
-else
+	yum install gcc make which
+	#yum groupinstall "Development Tools" gcc 
+elif [ "$os" = "deb" ];then
 	apt-get update && apt-get upgrade
-	apt-get install build-essential -y
+	#apt-get install build-essential -y
+	apt-get install make gcc
+elif [ "$os" = "pacman" ];then
+	pacman -Syy base-devel --noconfirm
+else
+   echo "OS NOT SUPPORTED BY THIS SCRIPT." >&2
+   exit 1 
 fi
+
 	
 make
 cd ..
@@ -132,47 +166,26 @@ chmod 700 vpncmd
 
 mkdir /var/lock/subsys
 
-touch /etc/init.d/"$initfile"
-#need to cat two time to pass varible($initfile) value inside
-cat > /etc/init.d/"$initfile" <<EOF
-#!/bin/sh
-# chkconfig: 2345 99 01
-# description: SoftEther VPN Server
-DAEMON=/usr/local/vpnserver/$initfile
-LOCK=/var/lock/subsys/$initfile
+touch /etc/systemd/system/"$service"
+#need to cat two time to pass varible($service) value inside
+cat > /etc/systemd/system/"$service" <<EOF
+[Unit]
+Description=SoftEther VPN Server
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/local/vpnserver/vpnserver start
+ExecStop=/usr/local/vpnserver/vpnserver stop
+
+[Install]
+WantedBy=multi-user.target
 EOF
 
-cat >> /etc/init.d/"$initfile" <<'EOF'
-test -x $DAEMON || exit 0
-case "$1" in
-start)
-$DAEMON start
-touch $LOCK
-;;
-stop)
-$DAEMON stop
-rm $LOCK
-;;
-restart)
-$DAEMON stop
-sleep 3
-$DAEMON start
-;;
-*)
-echo "Usage: $0 {start|stop|restart}"
-exit 1
-esac
-exit 0
-EOF
-
-chmod 755 /etc/init.d/"$initfile"
-if [ "$os" -eq "cent" ];then
-	chkconfig --add "$initfile" 
-	/etc/init.d/"$initfile" start
-else
-	update-rc.d "$initfile" defaults
-	/etc/init.d/"$initfile" start
-fi
+chmod 664 /etc/systemd/system/"$service"
+systemctl daemon-reload
+systemctl enable "$service"
+systemctl start "$service"
 	
 
 
@@ -180,8 +193,9 @@ echo "--------------------------------------------------------------------"
 echo "--------------------------------------------------------------------"
 echo "Installation done. Hurray."
 echo "Now you may want to change VPN server password."
+echo "See this image https://linuxconfig.org/images/set-admin-password-for-vpncmd.png"
 echo "Run in terminal:"
-echo "./vpncmd"
+echo "/usr/local/vpnserver/vpncmd"
 echo "Press 1 to select \"Management of VPN Server or VPN Bridge\","
 echo "then press Enter without typing anything to connect to the "
 echo "localhost server, and again press Enter without inputting "
@@ -189,4 +203,3 @@ echo "anything to connect to server by server admin mode."
 echo "Then use command below to change admin password:"
 echo "ServerPasswordSet"
 echo "Done...."
-
